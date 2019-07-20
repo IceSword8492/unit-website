1c8d187b7e0463c72b2e060d57425cbc1f7f5a28
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-07-20 19:44:01 +0900

    scorelist -> scoreboard

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
 
-    if (document.getElementById("scorelist"))
+    if (document.getElementById("scoreboard"))
     {
-        const table = document.getElementById("scorelist");
+        const table = document.getElementById("scoreboard");
         table.innerHTML += data.map(function (entry) {
             return "<tr><td>" + entry.name + "</td><td>" + entry.score + "</td></tr>";
         }).join("\n");
